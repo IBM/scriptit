dc7a8ee6ce5f65f41a66b392a4c63887e2490e96
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-02-10 05:16:25 +0600

    Update run_tests.sh
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+# Exit immediately if a command exits with a non-zero status
 set -e
+
+# Set the BASE_DIR to the parent directory of the script
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+# Change directory to the BASE_DIR
 cd "$BASE_DIR"
 
-PYTHONPATH="${BASE_DIR}:$PYTHONPATH" python3 -m pytest \
+# Set PYTHONPATH to include BASE_DIR for importing modules correctly
+PYTHONPATH="${BASE_DIR}:$PYTHONPATH" 
+
+# Run pytest with coverage checks
+python3 -m pytest \
     --cov-config=.coveragerc \
     --cov=scriptit \
     --cov-report=term \
