8ef3a812ae24efd0474194b1a6db6f5d1adc1026
Gabe Goodhart <ghart@us.ibm.com>
2024-02-09 14:58:29 -0700

    main: Use enum for colors and expose public utils in library
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

---
+++ scriptit/__init__.py
@@ -0,0 +1,15 @@
+"""
+Scriptit is a collection of tools for writing interactive terminal applications
+in a uinx terminal.
+"""
+
+# Local
+from .term_utils import (
+    Colors,
+    ascii_box,
+    ascii_table,
+    bg_colorize,
+    colorize,
+    decolorize,
+    progress_bar,
+)
