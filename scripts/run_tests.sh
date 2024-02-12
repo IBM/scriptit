b4ad8c02a7e24212341b830162728bf03ab991ce
Gabe Goodhart <ghart@us.ibm.com>
2024-02-12 13:26:17 -0700

    main: Add passing test threshold
    You've got to start somewhere!
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,10 +4,11 @@ set -e
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$BASE_DIR"
 
+fail_under=${FAIL_UNDER:-"29"}
 PYTHONPATH="${BASE_DIR}:$PYTHONPATH" python3 -m pytest \
     --cov-config=.coveragerc \
     --cov=scriptit \
     --cov-report=term \
     --cov-report=html \
-    --cov-fail-under=100 \
+    --cov-fail-under=$fail_under \
     $warn_arg "$@"
