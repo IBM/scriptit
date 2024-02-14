27dc5e18e7b28a1bf0b3978410dcd7650e877ed3
Gabe Goodhart <ghart@us.ibm.com>
2024-02-14 12:27:14 -0700

    AppTests: Use a resettable StringIO in printer tests
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- tests/test_refresh_printer.py
+++ tests/test_refresh_printer.py
@@ -3,10 +3,11 @@ Tests for RefreshPrinter
 """
 # Standard
 from unittest import mock
-import io
 
 # Third Party
 import pytest
+
+from tests.conftest import ResettableStringIO
 
 # Local
 from scriptit import RefreshPrinter
@@ -14,7 +15,7 @@ from scriptit import RefreshPrinter
 
 def test_refresh_printer_multi_line():
     """Test that adding multiple lines refreshes correctly"""
-    stream = io.StringIO()
+    stream = ResettableStringIO()
     printer = RefreshPrinter(write_stream=stream)
 
     # Perform initial add and refresh
@@ -25,6 +26,7 @@ def test_refresh_printer_multi_line():
     printed_lines = stream.getvalue().split("\n")
     assert len(printed_lines) == 3  # Two plus final empty line
     assert not any(line.startswith(RefreshPrinter.UP_LINE) for line in printed_lines)
+    stream.reset()
 
     # Perform a second refresh and make sure clearing happens
     printer.add("Line three")
@@ -32,13 +34,13 @@ def test_refresh_printer_multi_line():
     printer.refresh()
     assert printer.refreshes == 2
     printed_lines = stream.getvalue().split("\n")
-    assert len(printed_lines) == 6  # First 2, clear, 2 new, final \n
-    assert printed_lines[2].count(RefreshPrinter.UP_LINE) == 3
+    assert len(printed_lines) == 4  # Clear, 2 new, final \n
+    assert printed_lines[0].count(RefreshPrinter.UP_LINE) == 3
 
 
 def test_refresh_printer_newline_in_add():
     """Make sure that a newline in add is handled as separate lines"""
-    stream = io.StringIO()
+    stream = ResettableStringIO()
     printer = RefreshPrinter(write_stream=stream)
 
     # Perform initial add and refresh
@@ -57,7 +59,7 @@ def test_refresh_printer_wrap_lines(wrap):
     term_width = 5
     term_size_mock.columns = term_width
     with mock.patch("shutil.get_terminal_size", return_value=term_size_mock):
-        stream = io.StringIO()
+        stream = ResettableStringIO()
         printer = RefreshPrinter(write_stream=stream)
 
         # Perform initial add and refresh
@@ -72,18 +74,19 @@ def test_clear_last_report_chars():
     """Make sure that shorter lines in an update don't retain characters from
     previous reports
     """
-    stream = io.StringIO()
+    stream = ResettableStringIO()
     printer = RefreshPrinter(write_stream=stream)
 
     # Perform initial add and refresh
     printer.add("Line one")
     printer.refresh()
+    stream.reset()
 
     # Perform a second with a shorter line and make sure that the line does not
     # have any of the previous report in it
     printer.add("two")
     printer.refresh()
     printed_lines = stream.getvalue().split("\n")
-    assert len(printed_lines) == 4  # First 1, clear, 1 new, final \n
-    assert printed_lines[1].count(RefreshPrinter.UP_LINE) == 2
-    assert printed_lines[2].strip() == "two"
+    assert len(printed_lines) == 3  # Clear, 1 new, final \n
+    assert printed_lines[0].count(RefreshPrinter.UP_LINE) == 2
+    assert printed_lines[1].strip() == "two"
